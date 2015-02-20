cf26bd477a6627406d0d315c22d8c05d864a6d82
Miroslav Hettes <mirrec@gmail.com>
2015-02-20 15:38:18 +0100

    Iteration 4

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,15 +1,49 @@
 class FizzBuzz
+  class Number3
+    def self.match?(number)
+      number % 3 == 0
+    end
 
-  def fizz_buzz(number)
-    return "fizzbuzz" if number % 15 == 0
-
-    if number % 3 == 0
+    def self.fizz_buzz(number)
       "fizz"
-    elsif number % 5 == 0
+    end
+  end
+
+  class Number5
+    def self.match?(number)
+      number % 5 == 0
+    end
+
+    def self.fizz_buzz(number)
       "buzz"
-    else
+    end
+  end
+
+  class Number15
+    def self.match?(number)
+      number % 15 == 0
+    end
+
+    def self.fizz_buzz(number)
+      "fizzbuzz"
+    end
+  end
+
+  class NumberDefault
+    def self.match?(number)
+      true
+    end
+
+    def self.fizz_buzz(number)
       number
     end
+  end
+
+  def fizz_buzz(number)
+    number_klass = [
+        Number15, Number5, Number3, NumberDefault
+    ].find{ |number_klass| number_klass.match?(number) }
+    number_klass.fizz_buzz(number)
   end
 
   def fizz_buzz_upto(number)
