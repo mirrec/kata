a2b347a996ad71ecdc9979aa2bc679281b00fad0
Miroslav Hettes <mirrec@gmail.com>
2015-02-20 15:05:38 +0100

    Iteration 2

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,15 +1,27 @@
 class FizzBuzz
+  attr_reader :displayer
+
+  def initialize(displayer = Kernel)
+    @displayer = displayer
+  end
+
   def print_up_to(number)
     1.upto(number) do |n|
       if n % 15 == 0
-        puts "fizzbuzz"
+        display "fizzbuzz"
       elsif n % 3 == 0
-        puts "fizz"
+        display "fizz"
       elsif n % 5 == 0
-        puts "buzz"
+        display "buzz"
       else
-        puts n
+        display n
       end
     end
+  end
+
+  private
+
+  def display(string)
+    displayer.puts(string)
   end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,41 +1,42 @@
 require 'fizzbuzz'
 
 RSpec.describe "fizzbuzz" do
-  it "prints number from 1 to 10 when number is 10" do
-    output = with_captured_stdout do
-      FizzBuzz.new.print_up_to(15)
+  class FakeDisplayer
+    attr_reader :values
+
+    def initialize
+      @values = []
     end
 
-
-    expect(output).to eq(
-                          [
-                              "1",
-                              "2",
-                              "fizz",
-                              "4",
-                              "buzz",
-                              "fizz",
-                              "7",
-                              "8",
-                              "fizz",
-                              "buzz",
-                              "11",
-                              "fizz",
-                              "13",
-                              "14",
-                              "fizzbuzz\n"
-                          ].join("\n")
-                      )
-  end
-
-  def with_captured_stdout
-    begin
-      old_stdout = $stdout
-      $stdout = StringIO.new('','w')
-      yield
-      $stdout.string
-    ensure
-      $stdout = old_stdout
+    def puts(argument)
+      values << argument.to_s
     end
   end
+
+  it "prints fizzbuzz number from 1 to 15 when 15 is given as parameter" do
+    output = FakeDisplayer.new
+    FizzBuzz.new(output).print_up_to(15)
+
+
+    expect(output.values).to eq(
+                                 [
+                                     "1",
+                                     "2",
+                                     "fizz",
+                                     "4",
+                                     "buzz",
+                                     "fizz",
+                                     "7",
+                                     "8",
+                                     "fizz",
+                                     "buzz",
+                                     "11",
+                                     "fizz",
+                                     "13",
+                                     "14",
+                                     "fizzbuzz"
+                                 ]
+                             )
+  end
+
 end
