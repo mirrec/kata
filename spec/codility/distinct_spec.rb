109bedb422acfb9d4e4f6b786b984c2db7313d6c
Miroslav Hettes <mirrec@gmail.com>
2016-01-17 20:08:05 +0100

    Add solution for distinct exercise

---
+++ spec/codility/distinct_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require 'codility/distinct'
+
+RSpec.describe Codility::Distinct do
+  include Codility::Distinct
+
+  it 'returns number of distinct numbers' do
+    a = [2,1,1,2,3,1]
+
+    expect(solution(a)).to eq(3)
+  end
+
+  it 'returns 0 if array is empty' do
+    expect(solution([])).to eq(0)
+  end
+
+  it 'works for negative numbers as well' do
+    expect(solution([-1, -2, 0, -2, 3])).to eq(4)
+  end
+end
