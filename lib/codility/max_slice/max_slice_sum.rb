d5275c9f2107ceefcb2f3a94d986047670c6cf6d
Miroslav Hettes <mirrec@gmail.com>
2016-01-19 18:55:08 +0100

    Make max slice sum solution more clear

--- lib/codility/max_slice/max_slice_sum.rb
+++ lib/codility/max_slice/max_slice_sum.rb
@@ -2,24 +2,15 @@ module Codility
   module MaxSlice
     module MaxSliceSum
       def solution(a)
-        max_sum = 0
-        max_sum_in_position = 0
+        max_sum_in_position = [a[0]]
 
-        has_positive = false
-        max_element = a[0]
+        a.each_with_index do |element, index|
+          next if index == 0
 
-        a.each do |element|
-          max_sum_in_position = [0, max_sum_in_position + element].max
-          max_sum = [max_sum, max_sum_in_position].max
-          max_element = [max_element, element].max
-          has_positive = has_positive || (element > 0)
+          max_sum_in_position[index] = [element, element + max_sum_in_position[index - 1]].max
         end
 
-        if has_positive
-          max_sum
-        else
-          max_element
-        end
+        max_sum_in_position.max
       end
     end
   end
