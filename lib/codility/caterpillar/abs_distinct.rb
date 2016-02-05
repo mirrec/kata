4a0c67da4994a42de7a029d2953cd14ec0a731af
Miroslav Hettes <mirrec@gmail.com>
2016-02-05 14:42:40 +0100

    Add better solution for abs distinct exercise

--- lib/codility/caterpillar/abs_distinct.rb
+++ lib/codility/caterpillar/abs_distinct.rb
@@ -2,6 +2,48 @@ module Codility
   module Caterpillar
     module AbsDistinct
       def solution(a)
+        distinct = 0
+
+        x = 0
+        y = a.length - 1
+
+        while x <= y
+          if a[x].abs < a[y].abs
+            last_y = a[y].abs
+
+            distinct += 1
+
+            while a[y].abs == last_y && x <= y
+              y -= 1
+            end
+          elsif a[x].abs > a[y].abs
+            last_x = a[x].abs
+
+            distinct += 1
+            # x+=1
+
+            while a[x].abs == last_x && x <= y
+              x+=1
+            end
+          elsif a[x].abs == a[y].abs
+            distinct += 1
+
+            last_y = a[y].abs
+            while a[y].abs == last_y && x <= y
+              y -= 1
+            end
+
+            last_x = a[x].abs
+            while a[x].abs == last_x && x <= y
+              x+=1
+            end
+          end
+        end
+
+        distinct
+      end
+
+      def solution_with_hash(a)
         numbers = {}
         a.each do |number|
           numbers[number.abs] = true
--- spec/codility/caterpillar/abs_distinct_spec.rb
+++ spec/codility/caterpillar/abs_distinct_spec.rb
@@ -8,4 +8,29 @@ RSpec.describe Codility::Caterpillar::AbsDistinct do
     a = [-5, -3, -1, 0, 3, 6]
     expect(solution(a)).to eq(5)
   end
+
+  it 'returns correct number for all positive' do
+    a = [1, 3, 6]
+    expect(solution(a)).to eq(3)
+  end
+
+  it 'returns correct count for all negative numbers' do
+    a = [-6, -3, -1]
+    expect(solution(a)).to eq(3)
+  end
+
+  it 'returns correct number if there are some sme numbers' do
+    a = [1, 1, 1, 1]
+    expect(solution(a)).to eq(1)
+  end
+
+  it 'returns correct number' do
+    a = [1, 2, 2, 3]
+    expect(solution(a)).to eq(3)
+  end
+
+  it 'returns correct number' do
+    a = [-3, -2, -2, 1]
+    expect(solution(a)).to eq(3)
+  end
 end
