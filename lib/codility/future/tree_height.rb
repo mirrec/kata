1ba7e2ed59b7d3446c4a1efec8615ea33d623077
Miroslav Hettes <mirrec@gmail.com>
2016-02-10 12:27:46 +0100

    Add solution for tree height exercise

---
+++ lib/codility/future/tree_height.rb
@@ -0,0 +1,32 @@
+module Codility
+  module Future
+    module TreeHeight
+      Tree = Struct.new(:x, :l, :r)
+
+      def solution(a)
+        depth = -1
+
+        children_in_current_level = []
+
+        if a
+          children_in_current_level.push(a)
+        end
+
+        while children_in_current_level.length > 0
+          depth += 1
+
+          children_in_next_level = []
+
+          children_in_current_level.each do |current_node|
+            children_in_next_level.push(current_node.l) if current_node.l
+            children_in_next_level.push(current_node.r) if current_node.r
+          end
+
+          children_in_current_level = children_in_next_level
+        end
+
+        depth
+      end
+    end
+  end
+end
---
+++ spec/codility/future/tree_height_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+require 'codility/future/tree_height'
+require 'benchmark'
+
+RSpec.describe Codility::Future::TreeHeight do
+  include Codility::Future::TreeHeight
+  Tree = Codility::Future::TreeHeight::Tree
+
+  it 'works' do
+    # (5, (3, (20, None, None), (21, None, None)), (10, (1, None, None), None))
+    tree = Tree.new(5,
+             Tree.new(3,
+                      Tree.new(20, nil, nil),
+                      Tree.new(21, nil, nil)),
+             Tree.new(10,
+                      Tree.new(1, nil, nil),
+                      nil
+             ))
+
+    expect(solution(tree)).to eq(2)
+  end
+
+  it 'returns -1 for empty tree' do
+    expect(solution(nil)).to eq(-1)
+  end
+
+  it 'works for huge input' do
+    left_node = Tree.new(1,nil,nil)
+    right_node = Tree.new(1,nil,nil)
+
+    20.times do |n|
+      left = Tree.new(n + 2,left_node.dup,right_node.dup)
+      right = Tree.new(n + 2,left_node.dup,right_node.dup)
+
+      left_node = left
+      right_node = right
+    end
+
+    root_node = Tree.new(-1, left_node, right_node)
+
+    expect(solution(root_node)).to eq(21)
+  end
+
+  it 'works for deepest tree' do
+    left_node = Tree.new(1,nil,nil)
+    500.times do |n|
+      left_node = Tree.new(n + 2, left_node.dup, nil)
+    end
+
+    root_node = Tree.new(-1, left_node)
+
+    expect(solution(root_node)).to eq(501)
+  end
+end
